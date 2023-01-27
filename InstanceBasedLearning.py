54a09ce8d72d0fdcafc5f2f0c82d87b413d709ad
GolnooshBabaei <72923445+GolnooshBabaei@users.noreply.github.com>
2023-01-27 11:45:03 +0100

    Update InstanceSHAP class

--- InstanceBasedLearning.py
+++ InstanceBasedLearning.py
@@ -1,3 +1,4 @@
+import shap
 from sklearn.model_selection import train_test_split
 from InstanceSHAP.Data_preprocessing import PrepareData
 from sklearn.ensemble import RandomForestClassifier
@@ -17,19 +18,23 @@ class INSTANCEBASEDSHAP:
 
     def read_data(self):
         data_class = PrepareData()
-        data = PrepareData.getdata(data_class)
+        data = PrepareData.getdata(data_class).iloc[:1000, :]
         ## To reduce the computation time, I use 0.3 of each class available in the dataset
         data = data.groupby('label', group_keys=False).apply(lambda x: x.sample(frac=0.3))
         x = data.drop('label', axis=1)
         y = data['label']
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, test_size=0.3, stratify=y)
+        self.x_train.reset_index(inplace=True)
+        self.x_test.reset_index(inplace=True)
         return self.x_train, self.x_test, self.y_train, self.y_test
 
     def get_model_predictions(self):
-        ml_model = RandomForestClassifier(random_state=123).fit(self.x_train, self.y_train)
-        self.pd_test = ml_model.predict_proba(self.x_test)[:, 1]
-        self.pd_train = ml_model.predict_proba(self.x_train)[:, 1]
-        return self.pd_test, self.pd_train
+        rf = RandomForestClassifier(random_state=123).fit(self.x_train, self.y_train)
+
+        self.pd_test = rf.predict_proba(self.x_test)[:, 1]
+        self.pd_train = rf.predict_proba(self.x_train)[:, 1]
+        sigma_predictedvalue_train = np.std(self.pd_train)
+        return self.pd_test, self.pd_train, sigma_predictedvalue_train
 
     def calc_kernel(self, u):
         kernel = (1 / np.sqrt(2 * np.pi)) * (1 / np.exp(0.5 * (u ** 2)))
@@ -38,15 +43,12 @@ class INSTANCEBASEDSHAP:
     def find_similarities(self):
         d = []
         for i in range(len(self.x_train)):
-            for j in range(len(self.x_test)):
-                d.append(self.pd_train[i] - self.pd_test[j])
-        d_arr = np.array(d)
-        d = pd.DataFrame(np.array_split(d_arr, len(self.x_train)))
+            d.append(self.pd_train[i] - self.pd_test)
+        d = pd.DataFrame(d)
         return d
 
     def run_model(self):
-        data = self.prepare_data()
-        mymodel = RandomForestClassifier(random_state=123)
+        hzero = ((4 / (3 + len(self.x_train))) ** 0.2) * self.get_model_predictions()[2]
         hratio = np.arange(0.25, 1.5, 0.1)
         indices = Index()
 
@@ -56,37 +58,21 @@ class INSTANCEBASEDSHAP:
         Number_of_similar_observations_in_the_InstanceSHAP = []
 
         for r in range(2):
-            ## generate a sample of data including 1000 observations
-            # sample = data.groupby('status').apply(lambda x: x.sample(500))
-            sample = data.groupby('status').apply(lambda x: x.sample(frac=0.0005375))
-            sample.reset_index(inplace=True, drop=True)
 
-            X = sample.drop('status', axis=1)
-            y = sample['status']
-            xtrain, xtest, ytrain, ytest = train_test_split(X, y, test_size=0.1, stratify=y)
-            xtrain.reset_index(inplace=True, drop=True)
-            xtest.reset_index(inplace=True, drop=True)
-            xtrain_ml = xtrain.drop('interest', axis=1)
-            xtest_ml = xtest.drop('interest', axis=1)
-            mymodel.fit(xtrain_ml, ytrain)
-            ytrain_pred = mymodel.predict_proba(xtrain_ml)[:, 1]
-            ytest_pred = mymodel.predict_proba(xtest_ml)[:, 1]
-            sigma_predictedvalue_train = np.std(ytrain_pred)
-            hzero = ((4 / (3 + len(xtrain))) ** 0.2) * sigma_predictedvalue_train
             ret = xtrain['interest']
             final_cv_ = []
 
             ##create d matrix
-            d = self.find_similarities(xtrain, xtest, ytrain_pred, ytest_pred)
+            d = self.find_similarities()
 
             ## find cv for all possible values of h
             for h in hratio:
                 h_ = hzero * h
-
                 final_cv = []
-                cv_mat = pd.DataFrame(np.zeros((len(xtrain), len(xtrain))))
-                r_cv_mat = pd.DataFrame(np.zeros((len(xtrain), len(xtrain))))
-                for j in range(len(xtrain)):
+                cv_mat = pd.DataFrame(np.zeros((len(self.x_train), len(self.x_train))))
+                r_cv_mat = cv_mat.copy()
+
+                for j in range(len(self.x_train)):
                     for k in range(len(xtrain)):
                         cv_mat.iloc[j, k] = self.calc_kernel((ytrain_pred[j] - ytrain_pred[k]) / h_)
                         r_cv_mat.iloc[j, k] = cv_mat.iloc[j, k] * ret[k]
@@ -143,5 +129,5 @@ if __name__ == '__main__':
     model = c.get_model_predictions()
     d = c.find_similarities()
 
-    print(d)
+    print(d.head())
     print(d.shape)
